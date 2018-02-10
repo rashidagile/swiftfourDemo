11f4b7b2050d203398c1b1fa2fc31f28084ca164
agile-m-32 <rashid.shethwala@agileinfoways.com>
2018-02-10 14:10:32 +0530

    commit code 2 - 10-02-2018

--- swiftfourDemo/AppDelegate.swift
+++ swiftfourDemo/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    //test appdelegate
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
