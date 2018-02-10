b2e0fb755ad9254961378b344bdd271bb1f09555
agile-m-32 <rashid.shethwala@agileinfoways.com>
2018-02-10 14:40:50 +0530

    test comment 2:38

--- swiftfourDemo/ViewController.swift
+++ swiftfourDemo/ViewController.swift
@@ -63,6 +63,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        print("test commit....")
         
         if(self.blockTableViewDidSelectAtIndexPath != nil)
         {
